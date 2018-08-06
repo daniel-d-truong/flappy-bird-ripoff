4a5f3dda71534198a2cf2a34465d0e8ec3ebb0b2
Daniel Truong <ddtruong06@gmail.com>
2018-08-05 20:02:31 -0700

    Created bird and set up velocity/gravity var

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,9 +1,22 @@
+let bird;
+
+
 function setup() {
   // put setup code here
   createCanvas(400,600);
+  bird = new Bird();
 }
 
 function draw() {
   // put drawing code here
   background(0);
+  bird.update();
+  bird.show();
+}
+
+function keyPressed() {
+  if (key == ' '){
+    //console.log("SPACE");
+    bird.up();
+  }
 }
